9e25b42fb975af8d7e9db581b0cc8884461db8c3
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-02-12 19:08:03 +0100

    Add files via upload

---
+++ Combobox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "odabrana stavka:" + comboBox1.SelectedItem.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                textBox1.Text = "odabrana stavka:" + comboBox1.SelectedItem.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("odaberi stavku iz ComboBox-a", "informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
